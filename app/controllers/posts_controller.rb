2903dd61046df96c441ce656ff06b33e1db3efb8
kiboma <omayioben@gmail.com>
2023-06-23 07:37:17 +0300

    Redirect new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to movie_posts_url(@movie), notice: "Post was successfully created." }
+        format.html { redirect_to movie_url(@movie), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
